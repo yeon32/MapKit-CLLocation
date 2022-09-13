975d51fb70fc9688a2675d3b6518f9d1afeb5016
yeon32 <32jyeon@gamil.com>
2022-09-13 13:26:27 +0900

    Initial Commit

---
+++ MapKitCLLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKitCLLocation
+//
+//  Created by 김정연 on 2022/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
